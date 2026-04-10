ecda7121484b44ccafffbad56add50a5230f5b23
Mrityunjay <kmrityunjay450@gmail.com>
2026-04-11 01:09:10 +0530

    Fixed Request model case sensitivity

--- routes/requestRoutes.js
+++ routes/requestRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Request = require('../models/Request');
+const Request = require('../models/request');
 
 
 // 🟢 1. CREATE NEW REQUEST
