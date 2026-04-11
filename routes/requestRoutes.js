bc3a7e7f6ed468f88c4f5cba830980c7a8a919b6
Mrityunjay <kmrityunjay450@gmail.com>
2026-04-11 13:12:33 +0530

    Added automated Email Notifications for donors

--- routes/requestRoutes.js
+++ routes/requestRoutes.js
@@ -5,11 +5,14 @@ const User = require('../models/User'); // 🚀 Donors ki email nikalne ke liye 
 const nodemailer = require('nodemailer'); // 🚀 Email bhejne wala tool
 
 // 🚀 --- EMAIL SETUP SHURU --- 🚀
+const dns = require('dns');
+dns.setDefaultResultOrder('ipv4first');
+
 const transporter = nodemailer.createTransport({
   host: 'smtp.gmail.com',
-  port: 587,             // Standard port for Render
-  secure: false,         // true for 465, false for 587
-  requireTLS: true,      // Force safe connection
+  port: 587,
+  secure: false,
+  requireTLS: true,
   auth: {
     user: process.env.EMAIL_USER, 
     pass: process.env.EMAIL_PASS  
